3b8b3c240f6ee09b284b836b67ebdd830fa2e6ce
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-02-14 21:38:30 +0600

    Updated LoginView.java and added onhowver effect on the loginform.

--- src/main/java/com/cse471/project/views/login/LoginView.java
+++ src/main/java/com/cse471/project/views/login/LoginView.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.cookieconsent.CookieConsent;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.login.LoginForm;
@@ -21,19 +22,32 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
     public LoginView() {
         setSizeFull();
-        getStyle().set("background-color", "var(--lumo-contrast-5pct)")
+        addClassName("parent-login-view");
+        getStyle()
+                //.set("background-color", "var(--lumo-contrast-5pct)")
                 .set("display", "flex").set("justify-content", "center")
                 .set("padding", "var(--lumo-space-l)");
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
+        Div div = new Div();
+        login.getStyle().set("background-color", "var(--lumo-contrast-5pct)")
+                .set("display", "flex").set("justify-content", "center")
+                .set("padding", "var(--lumo-space-l)");
+        VerticalLayout vl = new VerticalLayout();
+        vl.setAlignItems(Alignment.CENTER);
+        vl.setJustifyContentMode(JustifyContentMode.CENTER);
+        div.add(vl);
+        div.addClassName("container");
         login.setAction("login");
-        login.setClassName("login-component", true);
+        login.setClassName("login-form", true);
         VerticalLayout header = new VerticalLayout();
+        header.addClassName("header");
         header.add(new H1("Lawyer Consultation"), new Span("This is our CSE471 Project"),
                 new Span("Currently we are just adding features."));
         header.setAlignItems(Alignment.CENTER);
         Button register = new Button("Register");
-        register.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_SUCCESS);
+        register.addClassName("button");
+        register.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
         var footer = new HorizontalLayout(new Span("New Here? "), register);
         footer.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         var cookie = new CookieConsent();
@@ -43,7 +57,8 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
         cookie.setLearnMoreLink("https://www.eatthis.com/surprising-side-effects-eating-cookies/");
         login.addForgotPasswordListener(forgotPasswordEvent -> UI.getCurrent().navigate("forgot-password"));
         register.addClickListener(buttonClickEvent -> UI.getCurrent().navigate("register-user"));
-        add(header, login, footer, cookie);
+        vl.add(header, login, footer, cookie);
+        add(div);
     }
 
 
