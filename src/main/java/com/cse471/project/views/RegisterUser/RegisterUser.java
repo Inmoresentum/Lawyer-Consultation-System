0b224e23403c1f5808fb553f4a56fff9dc215b3f
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-02-18 04:50:04 +0600

    Added validation in registration form and added some styles using css.

--- src/main/java/com/cse471/project/views/RegisterUser/RegisterUser.java
+++ src/main/java/com/cse471/project/views/RegisterUser/RegisterUser.java
@@ -3,18 +3,20 @@ package com.cse471.project.views.RegisterUser;
 import com.cse471.project.entity.Role;
 import com.cse471.project.entity.User;
 import com.cse471.project.service.UserService.UserService;
-import com.cse471.project.views.feedback.FeedbackView;
 import com.cse471.project.views.login.LoginView;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
@@ -36,16 +38,22 @@ public class RegisterUser extends VerticalLayout {
 
     private final TextField username = new TextField("Username");
     private final EmailField email = new EmailField("Email");
-    private PhoneNumberField phoneNumberField = new PhoneNumberField("Phone number");
+    private final PhoneNumberField phoneNumberField = new PhoneNumberField("Phone number");
     private final PasswordField password = new PasswordField("Password");
     private final PasswordField confirmPassword = new PasswordField("Confirm" +
             " Password");
     private final UserService userService;
 
     public RegisterUser(UserService userService) {
+        addClassName("r-v-registration-view");
+        setAlignItems(FlexComponent.Alignment.CENTER);
         this.userService = userService;
+        Div layout = new Div();
+        layout.addClassName("r-div");
         var header = new H3("Register");
+        header.addClassName("r-v-h3");
         var registerButton = new Button("Register", e -> register());
+        registerButton.addClassName("r-v-button");
         var link = new RouterLink("Already have an account? Login",
                 LoginView.class);
         // Adding the same css className so that I
@@ -55,13 +63,19 @@ public class RegisterUser extends VerticalLayout {
         setUpNameField();
         setUpPasswordFiled();
         setUpConfirmPasswordFiled();
-
-        add(header, new FormLayout(name, username, email, phoneNumberField, password, confirmPassword),
+        setEmailField();
+        FormLayout formLayout = new FormLayout(name, username, email, phoneNumberField,
+                password, confirmPassword);
+        formLayout.addClassName("r-v-form-layout");
+        layout.add(header, formLayout,
                 registerButton, link);
+        add(layout);
     }
 
     private void register() {
-        // Insert logic to save user data to a database or perform other actions
+        // Add register button login here to check if everything is correct or not
+        // If not correct, then show an error message
+        // based on the error.
         User user = new User();
         if (name.getValue() != null) {
             user.setName(name.getValue());
@@ -86,11 +100,23 @@ public class RegisterUser extends VerticalLayout {
                 .addThemeVariants(NotificationVariant.LUMO_SUCCESS);
     }
 
+    private void checkError() {
+    }
+
     private void setUpNameField() {
         name.setPlaceholder("Enter your name");
         name.setRequiredIndicatorVisible(true);
         name.setErrorMessage("You provide a valid username");
         name.setRequired(true);
+        name.addClassName("r-v-text-field");
+    }
+
+    private void setEmailField() {
+        email.setRequiredIndicatorVisible(true);
+        email.setPlaceholder("Enter your email");
+        email.setClearButtonVisible(true);
+        email.setErrorMessage("Please enter a valid email address");
+        email.addClassName("r-v-email-field");
     }
 
     private void setUpUserNameTextField() {
@@ -98,6 +124,7 @@ public class RegisterUser extends VerticalLayout {
         username.setRequiredIndicatorVisible(true);
         username.setErrorMessage("Username must be between 6-25 char");
         username.setRequired(true);
+        username.addClassName("r-v-text-field");
     }
 
     private void setUpPasswordFiled() {
@@ -106,6 +133,19 @@ public class RegisterUser extends VerticalLayout {
         password.setRequiredIndicatorVisible(true);
         password.setErrorMessage("The password must need to be a " +
                 "valid password between 1-16");
+        password.setMinLength(1);
+        password.setMaxLength(16);
+        password.addValueChangeListener(event -> {
+            if (!confirmPassword.isEmpty() &&
+                    !confirmPassword.getValue()
+                            .equals(password.getValue())) {
+                password.setErrorMessage("The passwords do not match");
+                password.setInvalid(true);
+            } else {
+                password.setInvalid(false);
+            }
+        });
+        password.addClassName("r-v-password-field");
     }
 
     private void setUpConfirmPasswordFiled() {
@@ -113,7 +153,14 @@ public class RegisterUser extends VerticalLayout {
         confirmPassword.setRequired(true);
         confirmPassword.setRequiredIndicatorVisible(true);
         confirmPassword.setErrorMessage("The passwords do not match");
-
+        confirmPassword.addValueChangeListener(event -> {
+            if (!confirmPassword.getValue().equals(password.getValue())) {
+                confirmPassword.setInvalid(true);
+                return;
+            }
+            confirmPassword.setInvalid(false);
+        });
+        addClassName("r-v-password-field");
     }
 
     private static class PhoneNumberField extends CustomField<String> {
