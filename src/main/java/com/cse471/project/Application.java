d2d7e234b59630f5982cc2a2dc7f7ad4e1c9baf7
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-02-20 20:43:21 +0600

    Turned the app into a progress web application using

--- src/main/java/com/cse471/project/Application.java
+++ src/main/java/com/cse471/project/Application.java
@@ -3,6 +3,7 @@ package com.cse471.project;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,6 +23,7 @@ import org.springframework.scheduling.annotation.Async;
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 @Async
+@PWA(name = "Lawyer Consultation", shortName = "Lawyer Consultation")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
