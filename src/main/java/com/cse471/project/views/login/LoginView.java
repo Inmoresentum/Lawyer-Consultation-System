125d8d8e5ad959101d3475749538c77293b2e315
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-03-17 03:44:53 +0600

    Added Router Links for TOC and P&P in the LoginView.java and further added some basic styles to the RouterLinks.

--- src/main/java/com/cse471/project/views/login/LoginView.java
+++ src/main/java/com/cse471/project/views/login/LoginView.java
@@ -1,5 +1,7 @@
 package com.cse471.project.views.login;
 
+import com.cse471.project.views.privacypolicy.PrivacyAndPolicyView;
+import com.cse471.project.views.termsandconditions.TermsAndConditionsView;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
@@ -40,8 +42,10 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
         register.addClassName("button");
         register.addThemeVariants(ButtonVariant.LUMO_PRIMARY,
                 ButtonVariant.LUMO_SUCCESS);
-        var footer = new HorizontalLayout(new Span("New Here? "), register);
-        footer.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        var userRegistrationPrompt =
+                new HorizontalLayout(new Span("New Here? "),
+                        register);
+        userRegistrationPrompt.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         var cookie = new CookieConsent();
         cookie.setCookieName("Analytic Cookies");
         cookie.setDismissLabel("Got it");
@@ -52,7 +56,20 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
                 navigate("forgot-password"));
         register.addClickListener(buttonClickEvent -> UI.getCurrent()
                 .navigate("register-user"));
-        vl.add(header, login, footer, cookie);
+        // Below adding the privacy-policy and Terms of condition Router Links
+        HorizontalLayout hl = new HorizontalLayout();
+        hl.addClassName("login-view-r-hl");
+        hl.setSpacing(false);
+        RouterLink termsOfServices = new RouterLink("Terms of Conditions",
+                TermsAndConditionsView.class);
+        termsOfServices.addClassName("login-view-tos");
+        RouterLink privacyAndPolicyView = new RouterLink("Privacy & Policy",
+                PrivacyAndPolicyView.class);
+        privacyAndPolicyView.addClassName("login-view-privacy-and-policy");
+        hl.add(termsOfServices, privacyAndPolicyView);
+        hl.setWidthFull();
+        hl.setJustifyContentMode(JustifyContentMode.BETWEEN);
+        vl.add(header, login, userRegistrationPrompt, hl);
         add(div);
     }
 
