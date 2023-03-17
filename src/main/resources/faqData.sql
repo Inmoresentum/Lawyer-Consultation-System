ec72d1d616e5f3874d3ee2040e049b4db9c00948
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-03-18 02:30:13 +0600

    Updated the faqData.sql and got rid of duplicates and added some more fqs. Also replaced the method with native sql.

--- src/main/resources/faqData.sql
+++ src/main/resources/faqData.sql
@@ -87,24 +87,57 @@ VALUES (70, 0, 'How do you decide whether you will take my case?',
         NOW(), NOW());
 
 INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
-VALUES (71, 0, 'How will I know what’s happening with my case?',
-        'We value your trust. We want you to be informed and involved. We are in touch with our clients frequently by e-mail, phone and in-person.',
-        NOW(), NOW());
-
-INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
-VALUES (72, 0, 'How long will it take before we go to trial?',
+VALUES (71, 0, 'How long will it take before we go to trial?',
         'We want to get your case to trial as soon as possible. However, the opposition may not want your case to get to trial quickly. We will need time to gather the evidence and develop your case',
         NOW(), NOW());
 
 INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
-VALUES (73, 0, 'Do I need a lawyer to represent me in court?',
+VALUES (72, 0, 'Do I need a lawyer to represent me in court?',
         'The ability to represent oneself in court is a harrowing task. A self-represented litigant is commonly unaware of legal professional procedure, court rules and legal precedent. There is no substitute for an opinion from a qualified professional about legal issues.',
         NOW(), NOW());
 
 INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
-VALUES (74, 0, 'What are your business hours?',
+VALUES (73, 0, 'What are your business hours?',
         'We are open From 8 A.M to 5 P.M on every Sunday to thursday.', NOW(), NOW());
 
 INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
-VALUES (75, 0, 'Will you charge for every consultation?',
+VALUES (74, 0, 'Will you charge for every consultation?',
         'No, our initial consultation is free.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (75, 0, 'How can I login to my account?',
+        'You can login to your account by entering your username and password on the login page.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (76, 0, 'I forgot my password. What should I do?',
+        'You can reset your password by clicking on the "Forgot Password" link on the login page and following the instructions.',
+        NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (77, 0, 'How long does an appointment usually take?',
+        'The length of an appointment depends on the type of service you are receiving. Generally, appointments range from 30 minutes to 2 hours.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (78, 0, 'How do I change my account information?',
+        'You can change your account information by logging into your account and clicking on the "Account Settings" button. From there, you can update your personal information.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (79, 0, 'How do I contact customer support?',
+        'You can contact customer support by emailing support@companyname.com or by calling our customer support hotline at 394893483948.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (80, 0, 'How do I deactivate my account?',
+        'You can deactivate your account by logging into your account and navigating to the "Settings" section. From there, you can select the "Deactivate Account" option and follow the instructions.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (81, 0, 'What should I do if I can''t access my account?',
+        'If you are unable to access your account, please contact customer service for assistance.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (82, 0, 'Can I have multiple accounts?',
+        'No, we only allow one account per user.', NOW(), NOW());
+
+INSERT INTO faq (id, version, faq_question, faq_answer, faq_created_at, faq_modified_at)
+VALUES (83, 0, 'What should I do if I experience technical issues?',
+        'If you experience technical issues, please contact customer service for assistance.', NOW(), NOW());
+
