a181b2396bc924debc2feefddac6755728ae02da
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-04-03 07:27:03 +0600

    Added LawyerRoleApplication.java where a user can submit an application which will be reviewed by the Admin and upon approving, the user will receive a Lawyer admin role. Currently, made all the necessary changes for the backend. Frontend will be done soon.

---
+++ src/main/java/com/cse471/project/entity/LawyerRoleApplicationStatus.java
@@ -0,0 +1,5 @@
+package com.cse471.project.entity;
+
+public enum LawyerRoleApplicationStatus {
+    ACCEPTED, REJECTED, ON_HOLD
+}
---
+++ src/main/java/com/cse471/project/entity/LawyerType.java
@@ -0,0 +1,14 @@
+package com.cse471.project.entity;
+
+public enum LawyerType {
+    Business_Lawyer, Bankruptcy_Lawyer, Tax_Lawyer, Defense_Lawyer,
+    Constitutional_Lawyer, Family_Lawyer, Labor_Lawyer, Estate_Planning_Lawyer,
+    Immigration_Lawyer, Personal_Injury_Lawyer, Intellectual_Property_Lawyer,
+    Entertainment_Lawyer, Medical_Malpractice_Lawyer, Contract_Lawyer,
+    Social_Security_Disability_Lawyer, Government_Lawyer, Military_Lawyer,
+    Mergers_and_Acquisitions_Lawyer, Environmental_Lawyer, Real_Estate_Attorney_or_Property_Lawyer,
+    Toxic_Tort_Lawyer, Video_Game_Lawyer, Public_Interest_Lawyer,
+    Digital_Media_and_Internet_Lawyer, Finance_and_Securities_Lawyer,
+    Civil_Rights_Lawyer, Workers_Compensation_Lawyer,
+    Civil_Litigation_Lawyer
+}
