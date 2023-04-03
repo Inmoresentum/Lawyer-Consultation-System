d79270bfa662dae965fb4e1037e8610de608c8e0
Laiba <faria.islam.laiba@g.bracu.ac.bd>
2023-04-04 04:44:00 +0600

    Added AboutView.java and About-view.css. Users can get a primary idea about the website from this about page.

--- src/main/java/com/cse471/project/views/about/AboutView.java
+++ src/main/java/com/cse471/project/views/about/AboutView.java
@@ -1,9 +1,7 @@
 package com.cse471.project.views.about;
 
 import com.cse471.project.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -23,15 +21,32 @@ public class AboutView extends VerticalLayout {
         img.setWidth("200px");
         add(img);
 
-        H2 header = new H2("This place intentionally left empty");
+        H1 header = new H1("What we do?");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        add(new Paragraph("Our motto is to take your legal action's personally. We believe in making your work or life secured." +
+                "Lawyer Consultancy is a professional legal platform. Here we will provide you proper guidelines for any" +
+                " legal issues. We are dedicated providing you the best service online. We hope you enjoy our lawyer's" +
+                " consultancy as much we enjoy offering them to you."));
+
+        H2 newHeader= new H2("Thanks for visiting our site");
+        newHeader.addClassNames("new-para");
+        add(newHeader);
+
+        H2 otherHeader = new H2("Have a nice day!");
+        otherHeader.addClassNames("other-para");
+        add(otherHeader);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
+
+
+
+
+
+
     }
 
 }
